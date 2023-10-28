58e6f9aa2dbcf4bca8a4e1ec7cd75e4ea8d5e827
sarachang3 <sarachang3@126.com>
2023-10-27 17:11:43 -0700

    Your commit message

--- app/src/main/java/com/example/cpen321mappost/MapsActivity.java
+++ app/src/main/java/com/example/cpen321mappost/MapsActivity.java
@@ -1,9 +1,7 @@
 package com.example.cpen321mappost;
 
 import androidx.fragment.app.FragmentActivity;
-
 import android.os.Bundle;
-
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -17,26 +15,18 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
+import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-
 import androidx.annotation.NonNull;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
-import androidx.fragment.app.FragmentActivity;
-
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.material.bottomsheet.BottomSheetDialog;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, LocationListener {
 
     private GoogleMap mMap;
     private ActivityMapsBinding binding;
@@ -44,24 +34,15 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private static final String TAG = "MapsActivity";
     private Marker selectedMarker = null;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         binding = ActivityMapsBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-
         locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
         requestLocationPermissions();
-
-
-        // Set onClickListener for the user profile button
         Button userProfileButton = findViewById(R.id.userProfileButton);
         userProfileButton.setOnClickListener(v -> {
             Intent intent = new Intent(MapsActivity.this, ProfileActivity.class);
@@ -72,67 +53,68 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private void requestLocationPermissions() {
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1234);
+        } else {
+            startLocationUpdates();
+        }
+    }
+
+    private void startLocationUpdates() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
         }
     }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults); // This line was missing
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         if (requestCode == 1234 && grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-            displayCurrentLocation();
+            startLocationUpdates();
         }
     }
-
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-        displayCurrentLocation();
-        // Handle click on map
         mMap.setOnMapClickListener(latLng -> {
-            // Remove selected marker
             if (selectedMarker != null) {
                 selectedMarker.remove();
             }
-
-            // Add new marker
             selectedMarker = mMap.addMarker(new MarkerOptions().position(latLng).title("Selected Location"));
         });
-
-        // Handle click on marker
         mMap.setOnMarkerClickListener(marker -> {
             if (selectedMarker != null && marker.equals(selectedMarker)) {
                 displayLocationMenu(marker.getPosition());
-                return true; // indicate that we consumed the event
+                return true;
             }
             return false;
         });
     }
+
     private void displayLocationMenu(LatLng latLng) {
         View view = getLayoutInflater().inflate(R.layout.layout_location_menu, null);
         BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(this);
         bottomSheetDialog.setContentView(view);
-
-        // Reference the button and set click listener
         Button createPostButton = view.findViewById(R.id.createPostButton);
         createPostButton.setOnClickListener(v -> {
-//            CreatePost(latLng);
             bottomSheetDialog.dismiss();
         });
-
         bottomSheetDialog.show();
     }
 
-    private void displayCurrentLocation() {
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-            Location lastKnownLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-            if (lastKnownLocation != null) {
-                LatLng currentLocation = new LatLng(lastKnownLocation.getLatitude(), lastKnownLocation.getLongitude());
-                Log.d("MapsActivity", "displayCurrentLocation():" + currentLocation.latitude + ", " + currentLocation.longitude);
-
-                mMap.addMarker(new MarkerOptions().position(currentLocation).title("Current Location"));
-                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLocation, 15));
-            }
-        }
+    @Override
+    public void onLocationChanged(Location location) {
+        LatLng currentLocation = new LatLng(location.getLatitude(), location.getLongitude());
+        mMap.addMarker(new MarkerOptions().position(currentLocation).title("Current Location"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLocation, 15));
     }
+
+    @Override
+    public void onProviderEnabled(String provider) {}
+
+    @Override
+    public void onProviderDisabled(String provider) {}
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {}
+
 }
