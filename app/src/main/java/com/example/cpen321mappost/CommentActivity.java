765cd54d32019cedf93a6949bda89935eb774c11
sarachang3 <sarachang3@126.com>
2023-10-29 17:57:11 -0700

    sara

--- app/src/main/java/com/example/cpen321mappost/CommentActivity.java
+++ app/src/main/java/com/example/cpen321mappost/CommentActivity.java
@@ -34,6 +34,7 @@ public class CommentActivity extends AppCompatActivity {
     private RecyclerView recyclerViewComments;
     private EditText editTextComment;
     private Button buttonSubmitComment;
+    private String pid;
 
     public interface JsonCallback<T> {
         void onSuccess(T result);
@@ -50,7 +51,7 @@ public class CommentActivity extends AppCompatActivity {
         buttonSubmitComment = findViewById(R.id.buttonSubmitComment);
 
         Intent receivedIntent = getIntent();
-        String pid = receivedIntent.getStringExtra("pid");
+        pid = receivedIntent.getStringExtra("pid");
 
         buttonSubmitComment.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -62,7 +63,7 @@ public class CommentActivity extends AppCompatActivity {
                     JSONObject postData = new JSONObject();
                     try {
                         postData.put("pid", pid);
-                        postData.put("uid", User.getInstance().getUserId());
+                        postData.put("userId", User.getInstance().getUserId());
                         postData.put("time", getCurrentDateUsingCalendar());
                         postData.put("content", newComment);
 
@@ -74,6 +75,9 @@ public class CommentActivity extends AppCompatActivity {
                         @Override
                         public void onSuccess(Void result) {
                             // TODO: Handle success of posting a comment
+                            Log.d(TAG, "Submit comment successfully");
+
+
                         }
 
                         @Override
@@ -89,18 +93,16 @@ public class CommentActivity extends AppCompatActivity {
     }
 
     public void displayAllComments(String pid) {
-        JSONObject postData = new JSONObject();
-        try {
-            postData.put("pid", pid);
-        } catch (JSONException e) {
-            throw new RuntimeException(e);
-        }
 
-        getAllCommentsData(postData, this, new JsonCallback<Comment[]>() {
+
+
+        getAllCommentsData(pid, this, new JsonCallback<Comment[]>() {
             @Override
             public void onSuccess(Comment[] comments) {
                 CommentAdapter commentAdapter = new CommentAdapter(comments);
                 recyclerViewComments.setAdapter(commentAdapter);
+                commentAdapter.notifyDataSetChanged(); // Refresh the RecyclerView
+
             }
 
             @Override
@@ -110,73 +112,149 @@ public class CommentActivity extends AppCompatActivity {
         });
     }
 
-    public void getAllCommentsData(JSONObject postData, final Activity activity, final JsonCallback<Comment[]> callback) {
-        String url = "http://4.204.251.146:8081/comments";
-        HttpUrl.Builder urlBuilder = HttpUrl.parse(url).newBuilder();
+    public void getAllCommentsData(String pid, final Activity activity, final JsonCallback<Comment[]> callback) {
 
-        Iterator<String> keys = postData.keys();
-        while (keys.hasNext()) {
-            String key = keys.next();
-            String value = postData.optString(key);
-            if (value != null) {
-                urlBuilder.addQueryParameter(key, value);
-            }
-        }
-
-        String fullUrl = urlBuilder.build().toString();
-        Request request = new Request.Builder().url(fullUrl).build();
+        String url = "http://4.204.251.146:8081/comments/?pid=" + pid;
 
         OkHttpClient httpClient = HttpClient.getInstance();
+
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+
         httpClient.newCall(request).enqueue(new Callback() {
             @Override
             public void onFailure(@NonNull Call call, @NonNull IOException e) {
-                activity.runOnUiThread(() -> callback.onFailure(e));
+                activity.runOnUiThread(() -> {
+                    Log.e(TAG, "Failed to get post data", e);
+                    callback.onFailure(e);
+                });
             }
-
             @Override
             public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
-                if (response.isSuccessful()) {
-                    try {
-                        String responseData = response.body().string();
-                        Gson gson = new Gson();
-                        Comment[] comments = gson.fromJson(responseData, Comment[].class);
-                        activity.runOnUiThread(() -> callback.onSuccess(comments));
-                    } finally {
-                        response.close();
+                activity.runOnUiThread(() -> {
+                    if (response.isSuccessful()) {
+                        try {
+                            if (response.code() == 200) {
+                                // The operation was successful.
+                                Log.d(TAG, "Data posted successfully!");
+                                String responseData = response.body().string();
+                                Gson gson = new Gson();
+                                Comment[] comments = gson.fromJson(responseData, Comment[].class);
+                                callback.onSuccess(comments); // already on UI thread, safe to call directly
+
+                            } else {
+                                // Handle other response codes (like 4xx or 5xx errors)
+                                IOException exception = new IOException("Unexpected response when posting data: " + response);
+                                Log.e(TAG, "Error posting data", exception);
+                                callback.onFailure(exception); // Notify callback about the failure
+                            }
+                        } catch (IOException e) {
+                            throw new RuntimeException(e);
+                        } finally {
+                            response.close(); // Important to avoid leaking resources
+                        }
+                    } else {
+                        IOException exception = new IOException("Unexpected code " + response);
+                        Log.e(TAG, "Error posting data", exception);
+                        callback.onFailure(exception); // Notify callback about the failure
                     }
-                } else {
-                    IOException exception = new IOException("Unexpected code " + response);
-                    activity.runOnUiThread(() -> callback.onFailure(exception));
-                }
+                });
             }
         });
     }
 
-    public void postCommentData(JSONObject postData, final Activity activity, final JsonCallback<Void> callback) {
+
+    public void postCommentData(JSONObject postData, final Activity activity, final CommentActivity.JsonCallback callback){
+
         String url = "http://4.204.251.146:8081/comments";
 
         OkHttpClient httpClient = HttpClient.getInstance();
+
+        // Convert the JSONObject to a string representation
         String jsonStrData = postData.toString();
+
+        Log.d(TAG, "This is the comment data: " + jsonStrData);
+
+        // Create a request body with the string representation of the JSONObject
         RequestBody body = RequestBody.create(jsonStrData, MediaType.parse("application/json; charset=utf-8"));
 
-        Request request = new Request.Builder().url(url).post(body).build();
+        // Build the request
+        Request request = new Request.Builder()
+                .url(url)
+                .post(body)
+                .build();
+
         httpClient.newCall(request).enqueue(new Callback() {
             @Override
             public void onFailure(@NonNull Call call, @NonNull IOException e) {
-                activity.runOnUiThread(() -> callback.onFailure(e));
+                activity.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Log.e(TAG, "Failed to post data", e);
+                        callback.onFailure(e); // Notify callback about the failure
+                    }
+                });
             }
 
             @Override
             public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
-                if (response.isSuccessful()) {
-                    activity.runOnUiThread(() -> callback.onSuccess(null));
-                } else {
-                    IOException exception = new IOException("Unexpected code " + response);
-                    activity.runOnUiThread(() -> callback.onFailure(exception));
-                }
+                activity.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        if (response.isSuccessful()) {
+                            try {
+                                if (response.code() == 200) {
+                                    // The operation was successful.
+                                    Log.d(TAG, "Data posted successfully!");
+                                    callback.onSuccess(null); // Notify callback about the success
+
+                                } else {
+                                    // Handle other response codes (like 4xx or 5xx errors)
+                                    IOException exception = new IOException("Unexpected response when posting data: " + response);
+                                    Log.e(TAG, "Error posting data", exception);
+                                    callback.onFailure(exception); // Notify callback about the failure
+                                }
+                            } finally {
+                                response.close(); // Important to avoid leaking resources
+                            }
+                        } else {
+                            IOException exception = new IOException("Unexpected code " + response);
+                            Log.e(TAG, "Error posting data", exception);
+                            callback.onFailure(exception); // Notify callback about the failure
+                        }
+                    }
+                });
             }
         });
     }
+
+//
+//    public void postCommentData(JSONObject postData, final Activity activity, final JsonCallback<Void> callback) {
+//        String url = "http://4.204.251.146:8081/comments";
+//
+//        OkHttpClient httpClient = HttpClient.getInstance();
+//        String jsonStrData = postData.toString();
+//        RequestBody body = RequestBody.create(jsonStrData, MediaType.parse("application/json; charset=utf-8"));
+//
+//        Request request = new Request.Builder().url(url).post(body).build();
+//        httpClient.newCall(request).enqueue(new Callback() {
+//            @Override
+//            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+//                activity.runOnUiThread(() -> callback.onFailure(e));
+//            }
+//
+//            @Override
+//            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+//                if (response.isSuccessful()) {
+//                    activity.runOnUiThread(() -> callback.onSuccess(null));
+//                } else {
+//                    IOException exception = new IOException("Unexpected code " + response);
+//                    activity.runOnUiThread(() -> callback.onFailure(exception));
+//                }
+//            }
+//        });
+//    }
 
 
     public String getCurrentDateUsingCalendar () {
