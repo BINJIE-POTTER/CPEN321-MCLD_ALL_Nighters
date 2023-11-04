1b5f545c27780ab50c0de674e130a5b5ef4848e1
Binjie Ye <yebinjie@student.ubc.ca>
2023-11-03 19:31:38 -0700

    fix post manager, binjie ye

--- app/src/main/java/com/example/cpen321mappost/PostManager.java
+++ app/src/main/java/com/example/cpen321mappost/PostManager.java
@@ -2,15 +2,18 @@ package com.example.cpen321mappost;
 
 import android.app.Activity;
 import android.util.Log;
+
 import androidx.annotation.NonNull;
+
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
-import com.google.gson.JsonSyntaxException;
 import com.google.gson.reflect.TypeToken;
+
 import java.io.IOException;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
+
 import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.MediaType;
