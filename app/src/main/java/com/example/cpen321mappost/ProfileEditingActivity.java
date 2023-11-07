030adfacca844d8c737cf0bd302649a088268c60
Binjie Ye <yebinjie@student.ubc.ca>
2023-11-06 17:49:44 -0800

    fix issue in ProfileEditingActivity, binjie ye

--- app/src/main/java/com/example/cpen321mappost/ProfileEditingActivity.java
+++ app/src/main/java/com/example/cpen321mappost/ProfileEditingActivity.java
@@ -1,20 +1,18 @@
 package com.example.cpen321mappost;
 
+import android.content.Intent;
+import android.os.Bundle;
 import android.os.Handler;
 import android.text.Editable;
 import android.text.TextWatcher;
-import android.view.View;
-import android.widget.Button;
-
-import com.google.android.material.textfield.TextInputEditText;
-
-import android.content.Intent;
-import android.os.Bundle;
 import android.util.Log;
+import android.widget.Button;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 import java.util.Objects;
 
