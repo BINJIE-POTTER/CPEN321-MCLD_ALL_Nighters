14b81b2ac48019b0738432f14be930961b335406
Leyang Pan <65788181+YuanSu526@users.noreply.github.com>
2023-11-30 17:19:01 -0800

    modifying frontend code to pass codacy

--- FrontEnd/app/src/main/java/com/example/cpen321mappost/Cluster.java
+++ FrontEnd/app/src/main/java/com/example/cpen321mappost/Cluster.java
@@ -107,14 +107,14 @@ class Post {
 // Getters and setters for image and other fields
 
 class ImageData {
-    public String getContentType() {
-        return contentType;
-    }
     private String contentType;
     private String image; // This holds the Base64 encoded image data
     public String getImage()
     {
         return image;
+    }
+    public String getContentType() {
+        return contentType;
     }
 
 }
